1e1b41bc1f5c2cbce2368820a7582b597c1c274f
Mike B Harris <mike.harris@xtreamlab.net>
2016-09-11 14:02:05 +0100

    First version of simple stack in COBOL
    A stack to use in COBOL programs implementing Push, Pop and Peek
    operators.

---
+++ Stack.cbl
@@ -0,0 +1,49 @@
+identification division.
+program-id. Stack.
+
+*> Implements a stack in COBOL
+*> Operators are: Pop, Push, and Peek
+
+data division.
+working-storage section.
+
+    01 StackTable is global.
+        02 StackItem pic x(20) occurs 1 to 50 times.
+    01 ItemsInStack pic 99 value zero is global.
+
+linkage section.
+
+    01 Command pic x any length.
+    01 Item pic x any length.
+
+procedure division using Command, Item.
+
+    evaluate function trim(Command)
+        when "Push" perform Push thru EndPush
+        when "Pop"  perform Pop thru EndPop
+        when "Peek" perform Peek thru EndPeek
+    end-evaluate
+    goback.
+
+Push.
+    add 1 to ItemsInStack
+    move Item to StackItem(ItemsInStack).
+EndPush.
+
+Peek.
+    *> Return last item pushed onto stack without removing it
+    move StackItem(ItemsInStack) to Item.
+EndPeek.
+
+Pop.
+    *> Return last item pushed onto stack and remove it
+    if ItemsInStack less than 1 then
+        move spaces to Item
+    else
+        move StackItem(ItemsInStack) to Item
+        move spaces to StackItem(ItemsInStack)
+        subtract 1 from ItemsInStack
+    end-if.
+EndPop.
+
+end program Stack.
---
+++ StackTest.cbl
@@ -0,0 +1,45 @@
+identification division.
+program-id. StackTest.
+
+*> A simple stack in COBOL
+
+environment division.
+
+data division.
+working-storage section.
+
+    01 Item pic x(10) value spaces.
+    01 Result pic x(10) value spaces.
+
+procedure division.
+
+    move "hello" to Item
+    call "Stack" using by content "Push", by content Item
+    move "hola" to Item
+    call "Stack" using by content "Push", by content Item
+    move "bonjour" to Item
+    call "Stack" using by content "Push", by content Item
+    call "Stack" using by content "Pop", by reference Result
+    call "AssertEquals" using Result, "bonjour", "Returned 3rd value put on stack"
+    call "Stack" using by content "Pop", by reference Result
+    call "AssertEquals" using Result, "hola", "Returned 2nd value put on stack"
+    call "Stack" using by content "Pop", by reference Result
+    call "AssertEquals" using Result, "hello", "Returned 1st value put on stack"
+    call "Stack" using by content "Pop", by reference Result
+    call "AssertEquals" using Result, "", "Read past end of stack returns spaces"
+
+    move 35 to Item
+    call "Stack" using by content "Push", by content Item
+    call "Stack" using by content "Push", by content "hola"
+    call "Stack" using by content "Pop", by reference Result
+    call "AssertEquals" using Result, "hola", "Returned 2nd new value put on stack"
+    call "Stack" using by content "Pop", by reference Result
+    call "AssertEquals" using Result, Item, "Returned 1st new numeric value put on stack"
+
+    call "Stack" using by content "Push", by content "hello"
+    call "Stack" using by content "Peek", by reference Result
+    call "AssertEquals" using Result, "hello", "Peeked value put on stack"
+    call "Stack" using by content "Peek", by reference Result
+    call "AssertEquals" using Result, "hello", "Peeked value returned again"
+
+stop run.
